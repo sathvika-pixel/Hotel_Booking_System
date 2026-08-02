becf64ee380c668e6e4f17260ac520f810ac838e
Sathvika <sathvikavvankadara@gmail.com>
2026-08-02 23:47:48 +0530

    Fix ALLOWED_HOSTS syntax

--- HotelBooking/settings.py
+++ HotelBooking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j5i=w7qxq^*lo6(topj2)xp+(w-8sl9zv-ufn_37v+395*pv0a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  [
+ALLOWED_HOSTS = [
     '.onrender.com',
     'localhost',
     '127.0.0.1'
